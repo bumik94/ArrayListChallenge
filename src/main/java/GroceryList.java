47029482b281a9fdc8d19c14c45b7d9087b13713
bumik94 <vaclav.himl@gmail.com>
2024-07-03 00:28:27 +0000

    Minor changes

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -65,7 +65,6 @@ public class GroceryList {
                 break;
             }
             System.out.println("""
-                    [----------------]
                     [Invalid number]
                     }_""");
         }
